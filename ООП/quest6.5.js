47c991ec0bdfe52b465637ba9249b998fc1b4b25
archanvel <disk.space456@gmail.com>
2021-01-26 14:39:56 +0200

    Update OOP

---
+++ ООП/quest6.5.js
@@ -0,0 +1,36 @@
+class User{
+    constructor(name, surname){
+        this.name = name;
+        this.surname = surname;
+    }
+    getFullName(){
+        return this.name + '  ' + this.surname;
+    }
+}
+
+class Student extends User{
+    constructor (name, surname, year){
+        super(year);
+        this.name = name;
+        this.surname = surname;
+        this.year = year;
+    }
+    getCourse(){
+        var fullyear = 2021;
+        var course = fullyear - this.year;
+        if (course > 1){
+            return(course);
+        }
+        else{
+            console.log('Error');
+        }
+    }
+}
+
+var student = new Student('Иван', 'Иванов', 2018);
+
+console.log(student.name); //выведет 'Иван'
+console.log(student.surname); //выведет 'Иванов'
+console.log(student.getFullName()); //выведет 'Иван Иванов'
+console.log(student.year); //выведет 2018
+console.log(student.getCourse()); //выведет 3 - третий курс, так как текущий год 2021
