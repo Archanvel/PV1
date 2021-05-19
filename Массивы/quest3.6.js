4d32c7d36b0c4eca3d1c2a55173e26100a91407c
Archanvel <disk.space456@gmail.com>
2021-05-19 14:50:33 +0300

    Update final

--- Массивы/quest3.6.js
+++ Массивы/quest3.6.js
@@ -1,3 +1,3 @@
-const arr = [1, 2, 3, 4 , 5 , 6 , 7 , 8 , 9]
-arrResult = arr.slice(0, 4).concat(0 , 0 , 0).concat(arr.slice(5));
-alert(arrResult);
+const numbers = [1, 2, 3, 4 , 5 , 6 , 7 , 8 , 9] // Создаем заданный массив
+numbersResult = numbers.slice(0, 4).concat(0 , 0 , 0).concat(numbers.slice(5));
+alert(numbersResult);
--- ООП/quest6.2.js
+++ ООП/quest6.2.js
@@ -1,17 +1,17 @@
-class Person {
-    constructor (name, surname) {
+class Person { // Создаем класс Person
+    constructor (name, surname) { // Создаем конструктор класса Person
         this.name = name;
         this.surname = surname;
     }
-    getName(){
+    getName(){ // Функция для возвращения имени
         return this.name;
     }
-    getSurname(){
+    getSurname(){ // Функция для возвращения фамилии
         return this.surname;
     }
 }
 
-var worker = new Person('Иван', 'Иванов');
+var worker = new Person('Иван', 'Иванов'); // Создаем обьект worker класса Person и передаем параметры
 
 console.log(worker.getName()); //выведет 'Иван'
 console.log(worker.getSurname()); //выведет 'Иванов'
