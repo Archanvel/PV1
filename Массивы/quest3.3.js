bbc6b7467fcbd72d6048918e8e2b26d455220679
Archanvel <disk.space456@gmail.com>
2021-05-19 14:07:37 +0300

    New update

--- Массивы/quest3.3.js
+++ Массивы/quest3.3.js
@@ -1,5 +1,5 @@
-let styles = ["Джаз", "Блюз"];
-styles.push("Рок-н-ролл");
+let styles = ["Джаз", "Блюз"]; // Создаем массив
+styles.push("Рок-н-ролл"); // Добавляем в конец массива "Рок-н-ролл"
 styles[Math.floor((styles.length - 1) / 2)] = "Классика";
 alert( styles.shift() );
 styles.unshift("Рэп", "Регги");
