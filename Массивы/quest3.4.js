13434b2df60b33fff2e9a22002b6eeb5afcaddd7
Archanvel <disk.space456@gmail.com>
2021-05-19 14:36:59 +0300

    New update

--- Массивы/quest3.4.js
+++ Массивы/quest3.4.js
@@ -1,19 +1,19 @@
 function sumInputNumbers() {
 
-    let numbers = [];
+    let numbers = []; // Создаем массив с числами
+    let sum; // Переменная для хранения суммы элементов массива
   
     while (true) {
   
-      let value = prompt("Число", 0);
+      let value = prompt("Число"); // Ввод чисел с клавиатуры
   
-      if (value === "" || value === null || !isFinite(value)) break;
+      if (value == "" || value == null || !isFinite(value)) break;   // isFinite проверяет является ли числом
   
-      numbers.push(+value);
+      numbers.push(+value); // Заносим числа в конец массива
     }
   
-    let sum = 0;
-    for (let number of numbers) {
-      sum += number;
+    for (i=0; i < numbers.push; i++) { // Цикл для сложения элементов массива
+      sum += numbers.push[i] ; // Сумма всех элементов массива
     }
     return sum;
   }
